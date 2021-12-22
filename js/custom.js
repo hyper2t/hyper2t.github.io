f08a13b78975d2272f05b7e69f3378f75a5671b3
hyper2t <hyper2t@users.noreply.github.com>
2021-12-22 15:13:45 +0000

    add background10 hyper2t/hyper2t.github.io.source@6ed833361ece01072cefafab428865fdc8695f59

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,7 @@ $(function () {
     $.backstretch([
         "/background2.jpeg",
         "/background8.jpeg",
+        "/background10.jpeg",
         "/background1.jpeg"
     ], { duration: 60000, fade: 1500 });
 });
